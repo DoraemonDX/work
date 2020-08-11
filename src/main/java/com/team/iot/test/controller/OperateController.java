c484360537af8bd7022172b0f45937098b52a0ad
wcl925 <69347934+wcl925@users.noreply.github.com>
2020-08-11 13:27:08 +0800

    Update OperateController.java
    queryOperateById

--- src/main/java/com/team/iot/test/controller/OperateController.java
+++ src/main/java/com/team/iot/test/controller/OperateController.java
@@ -21,8 +21,8 @@ public class OperateController {
     Gson gson = new Gson();
 
     @RequestMapping("/addOperate")
-    public String addOperate(int id, String customer, String reservation_time, String run_time, String road, String driver, String team, String reality_plate, String applition_plate, String models, String appition_earn, String tax, String appition_pay, String time, String yuetongbao, String road_cost, String oil_cost, String play_cost, String niaosu_cost, String other_cost, String serial_number, String note){
-        Operate operate = new Operate(id, customer, reservation_time, run_time, road, driver, team, reality_plate, applition_plate, models,appition_earn,tax,  appition_pay,  time,  yuetongbao, road_cost, oil_cost,play_cost,niaosu_cost,other_cost,serial_number,note,new Date());
+    public String addOperate(int id, String customer, String reservation_time, String run_time, String road, String driver, String team, String reality_plate, String applition_plate, String models, String appition_earn, String tax, String appition_pay, String time, String yuetongbao, String road_cost, String oil_cost, String play_cost, String niaosu_cost, String other_cost, String serial_number ,String state, String note){
+        Operate operate = new Operate(id, customer, reservation_time, run_time, road, driver, team, reality_plate, applition_plate, models,appition_earn,tax,  appition_pay,  time,  yuetongbao, road_cost, oil_cost,play_cost,niaosu_cost,other_cost,serial_number,note,state,new Date());
         return gson.toJson(operateMapper.addOperate(operate));
     }
 
@@ -32,19 +32,24 @@ public class OperateController {
     }
 
     @RequestMapping("/updateOperate")
-    public String updateOperate(int id, String customer, String reservation_time, String run_time, String road, String driver, String team, String reality_plate, String applition_plate, String models, String appition_earn, String tax, String appition_pay, String time, String yuetongbao, String road_cost, String oil_cost, String play_cost, String niaosu_cost, String other_cost, String serial_number, String note){
-        Operate operate = new Operate(id, customer, reservation_time, run_time, road, driver, team, reality_plate, applition_plate, models,appition_earn,tax,  appition_pay,  time,  yuetongbao, road_cost, oil_cost,play_cost,niaosu_cost,other_cost,serial_number,note,new Date());
+    public String updateOperate(int id, String customer, String reservation_time, String run_time, String road, String driver, String team, String reality_plate, String applition_plate, String models, String appition_earn, String tax, String appition_pay, String time, String yuetongbao, String road_cost, String oil_cost, String play_cost, String niaosu_cost, String other_cost, String serial_number, String state, String note){
+        Operate operate = new Operate(id, customer, reservation_time, run_time, road, driver, team, reality_plate, applition_plate, models,appition_earn,tax,  appition_pay,  time,  yuetongbao, road_cost, oil_cost,play_cost,niaosu_cost,other_cost,serial_number,note,state,new Date());
         return gson.toJson(operateMapper.updateOperate(operate));
     }
 
     @RequestMapping("/operateList")
     public String queryOperateList(){
-       return gson.toJson(operateMapper.queryOperateList());
+        return gson.toJson(operateMapper.queryOperateList());
     }
 
     @RequestMapping("/queryOperateById")
     public String queryOperateById(int id){
         return gson.toJson(operateMapper.queryOperateById(id));
+    }
+
+    @RequestMapping("/queryOperateListById")
+    public String queryOperateById(String state){
+        return gson.toJson(operateMapper.queryOperateListById(state));
     }
 }
 
